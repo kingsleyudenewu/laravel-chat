1852b29049ddf525b797a8a3122d1ba3a0d13e10
kingsleyudenewu <kingzpacking@gmail.com>
2024-07-29 10:19:45 +0100

    Add action class to create a user token

--- app/Actions/RegisterAction.php
+++ app/Actions/RegisterAction.php
@@ -2,18 +2,41 @@
 
 namespace App\Actions;
 
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
 class RegisterAction
 {
-    public function execute(array $data)
+    /**
+     * Register a new user into the blog system
+     *
+     * @param array $data
+     * @return array
+     */
+    public function execute(array $data): array
     {
         return DB::transaction(function () use ($data) {
             $user = $this->createUser($data['name'], $data['email'], $data['password']);
 
-            $this->createUserLedger($user);
-
             return (new GenerateTokenAction())->execute($user);
         });
+    }
+
+    /**
+     * Create a user
+     *
+     * @param string $name
+     * @param string $email
+     * @param string $password
+     * @return User
+     */
+    private function createUser(string $name, string $email, string $password): User
+    {
+        return User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'email_verified_at' => now(),
+        ]);
     }
 }
